3df5daa58b3850ba3e67031e6c5fba3ffe89653d
Jake Goldsborough <jake.goldsborough@pm.me>
2026-02-02 15:41:48 -0800

    fix: force dynamic rendering for browse page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { getAllUsers, getDetectionsForUser, getToolStats } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export default function BrowsePage() {
   const users = getAllUsers(50);
   const toolStats = getToolStats();
